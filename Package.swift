9727df6c6a00e1c08d8b4abd7c8b1ae9b4960986
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-06-30 12:35:41 +0500

    Update package manifest to latest versions
    - Swift 5.3
    - iOS and tvOS 14
    - macOS 11, 10.16 rounded up :)

--- Package.swift
+++ Package.swift
@@ -1,10 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "OctopusKitLogViewer",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_16),
+        .tvOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
